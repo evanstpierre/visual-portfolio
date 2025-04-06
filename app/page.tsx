1cdfb5a6e098683b94804c51820b3015158a4dc3
evanst.pierre <e3stpier@uwaterloo.ca>
2025-04-06 16:50:25 -0400

    add underlien to buttons

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,15 @@ export default function Home() {
       <div className="absolute inset-0 flex flex-col z-0">
         {/* Top Half */}
         <div className="flex-1 bg-[#F5EFE7] relative">
-          <div className="absolute top-10 left-20  hover:underline cursor-pointer transition-all duration-500"  onClick={() => window.location.href = "mailto:sarahepperle@gmail.com"} ><span className="top">Contact</span></div>
-          <div className="absolute top-10 right-20  hover:underline cursor-pointer transition-all duration-500" onClick={() => setShowAbout(true)}><span className="top">About</span></div>
+          <div className="absolute top-10 left-20  "  onClick={() => window.location.href = "mailto:sarahepperle@gmail.com"} ><span className="top border-b-1 border-transparent hover:border-[#3E5879] transition-all duration-500 cursor-pointer">Contact</span></div>
+          <div className="absolute top-10 right-20  h" onClick={() => setShowAbout(true)}><span className="top border-b-1 border-transparent hover:border-[#3E5879] transition-all duration-500 cursor-pointer">About</span></div>
         </div>
 
         {/* Bottom Half */}
         <div className="flex-1 bg-[#3E5879] relative">
-          <div className="absolute bottom-10 left-20 hover:underline cursor-pointer transition-all duration-500"><span className="bottom text-white">Resume</span></div>
-          <div className="absolute bottom-10 right-20 hover: cursor-pointer transition-all duration-500" onClick={() => setShowWork(true)}><span className="bottom text-white">Work</span></div>
-        </div>underline
+          <div className="absolute bottom-10 left-20"><span className="bottom border-b-1 border-transparent hover:border-[#F5EFE7] transition-all duration-500 cursor-pointer">Resume</span></div>
+          <div className="absolute bottom-10 right-20" onClick={() => setShowWork(true)}><span className="bottom border-b-1 border-transparent hover:border-[#F5EFE7] transition-all duration-500 cursor-pointer">Work</span></div>
+        </div>
       </div>
 
       {/* Centered Box + Header Wrapper */}
