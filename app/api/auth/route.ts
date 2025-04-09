9879632dea6c0e59c1d8ccb2a335ecf0ec3d1d23
evanst.pierre <e3stpier@uwaterloo.ca>
2025-04-09 08:20:21 -0400

    add auth and login API

---
+++ app/api/auth/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+
+export async function GET(req: NextRequest) {
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    return NextResponse.json({ message: 'Missing JWT secret' }, { status: 500 });
+  }
+
+  const authHeader = req.headers.get('authorization');
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return NextResponse.json({ message: 'No token provided' }, { status: 401 });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    return NextResponse.json({ user: decoded }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: 'Invalid or expired token' }, { status: 401 });
+  }
+}
--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -23,44 +23,12 @@ export async function POST(req: NextRequest) {
   if (!isValid) {
     return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
   }
+  const token = jwt.sign(
+    { id: user.id, role:"ADMIN" },
+    process.env.JWT_SECRET!,
+    { expiresIn: '1h' }
+  );
 
-  return NextResponse.json({ message: 'Login successful' });
+  return NextResponse.json({ message: 'Login successful',  token });
 }
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   const ADMIN_ID = 1
-//   if (req.method !== 'POST') {
-//     return res.status(405).json({ message: 'Method not allowed' });
-//   }
-
-//   const {password } = req.body;
-
-//   if ( !password) {
-//     return res.status(400).json({ message: 'Missing password' });
-//   }
-
-//   const user = users.find(u => u.id === ADMIN_ID);
-//   if (!user) {
-//     return res.status(401).json({ message: 'Invalid login' });
-//   }
-  
-//   const isValid = await bcrypt.compare(password, user.passwordHash);
-
-//   if (!isValid) {
-//     return res.status(401).json({ message: 'Invalid login' });
-//   }
-
-//   const token = jwt.sign(
-//     { id: user.id, },
-//     process.env.JWT_SECRET!, // set in .env.local
-//     { expiresIn: '30m' }
-//   );
-  
-//   // ✅ Success — return a token or session info
-//   return res.status(200).json({
-//     message: 'Login successful',
-//     token: token,
-//     // user: { id: user.id, email: user.email },
-//     // optionally include a JWT token here
-//   });
-// }
